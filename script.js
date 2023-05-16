33e228062124375195198c6aff7a53d99f92b341
Alexandre Carvalho <alex380bb@gmail.com>
2023-05-16 10:58:11 -0300

    exercicios requesito 1

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ createDaysOfTheWeek();
 const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
 // Escreva seu código abaixo.
+
 const criandoDiasDoMes = () => {
   const listaDeDias = document.querySelector('#days');
 
@@ -29,11 +30,9 @@ const criandoDiasDoMes = () => {
     if (day === 24 || day === 25 || day === 31) {
       dayItem.classList.add('holiday');
     }
-    
     if (day === 4 || day === 11 || day === 18 || day === 25) {
       dayItem.classList.add('friday');
     }
-
     listaDeDias.appendChild(dayItem);
   }
 }
